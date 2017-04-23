dfaee578a386dfd806d3edff4b877b4f43746927
HieuNM6 <hieunmce@gmail.com>
2017-04-23 22:59:57 +0700

    add feature export image

--- js/app.js
+++ js/app.js
@@ -5,7 +5,12 @@ import 'waypoints';
 import 'scrollTo';
 import {fabric} from "fabric";
 
-
+let imagePosition = {
+  left: 0,
+  top: 0,
+  width: 0,
+  height: 0
+}
 window.canvas = new fabric.Canvas("canvas");
 var edgedetection = 8;
 canvas.selection = false;
@@ -23,6 +28,18 @@ resizeCanvas();
 
 // Initialize Everything
 init();
+
+$('#exportBtn').on('click', saveImg);
+
+function saveImg() {
+  window.open(canvas.toDataURL({
+    left: imagePosition.left,
+    top: imagePosition.top,
+    width: imagePosition.width,
+    height: imagePosition.height
+  }));
+}
+
 
 function init(top, left, width, height, fill) {
 
@@ -77,6 +94,30 @@ function init(top, left, width, height, fill) {
       strokeShadow: true
   };
 
+  var squareScreen = new fabric.Rect({
+      top: 100,
+      left: 100,
+      width: 400,
+      height: 400,
+      fill : '#f55',
+      lockRotation: true,
+      originX: 'left',
+      originY: 'top',
+      cornerSize: 15,
+      hasRotatingPoint: false,
+      perPixelTargetFind: true,
+      stroke: 'black',
+      strokeWidth: 1,
+      selectable: false
+  });
+
+
+  imagePosition.height = squareScreen.height;
+  imagePosition.left = squareScreen.left;
+  imagePosition.top = squareScreen.top;
+  imagePosition.width = squareScreen.width;
+
+  window.canvas.add(squareScreen);
   window.canvas.add(bg);
   bg.setShadow(shadow);
   window.canvas.add(squareBtn);
@@ -88,6 +129,7 @@ function init(top, left, width, height, fill) {
 
   function draggable(object) {
     object.on('mousedown', function () {
+      // Add control to new object
       if (this.hasControls == false) {
         this.set({
           hasControls: true,
@@ -111,5 +153,4 @@ function init(top, left, width, height, fill) {
   draggable(squareBtn);
   draggable(circleBtn);
   draggable(triangleBtn);
-
 }
